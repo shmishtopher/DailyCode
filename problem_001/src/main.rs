2d653200b1ae211ad9e264862dd9adb18cae5385
Shmish <shmish90@gmail.com>
2019-05-27 19:09:12 -0400

    updated docs

--- problem_001/src/main.rs
+++ problem_001/src/main.rs
@@ -4,8 +4,8 @@
 
 //! # Problem 001
 //! 
-//! Given a list of numbers, return whether any two sums to k. For example, 
-//! given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+//! Given a list of numbers, return whether any two sums to `k`. For example, 
+//! given `[10, 15, 3, 7]` and `k` of `17`, return true since `10 + 7` is `17`.
 //!
 //! Bonus: Can you do this in one pass?
 
